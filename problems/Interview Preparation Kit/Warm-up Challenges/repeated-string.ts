fdf58032c7a61040f56c3a0758db087f9f90147b
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-09-02 08:09:27 -0300

    Add url to repeated-string

--- problems/Interview Preparation Kit/Warm-up Challenges/repeated-string.ts
+++ problems/Interview Preparation Kit/Warm-up Challenges/repeated-string.ts
@@ -1,3 +1,5 @@
+// https://www.hackerrank.com/challenges/repeated-string/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+
 function repeatedString(s: string, n: number): number {
   // Write your code here
   

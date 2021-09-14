77c0e63694314d0bd42abe3bf5628b80f156c347
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-09-14 07:36:24 -0300

    add weatherObservationStation1

---
+++ problems/SQL/weatherObservationStation1.sql
@@ -0,0 +1,5 @@
+-- Query a list of CITY and STATE from the STATION table.
+-- Challenge URL: https://www.hackerrank.com/challenges/weather-observation-station-1/problem
+SELECT CITY,
+  STATE
+FROM STATION
